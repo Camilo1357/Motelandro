96001cde6a1ded8a5a274e3ad4250b772971e822
EmanuelQuintero51 <emanuelquinrerozuluaga51@gmail.com>
2026-02-27 22:46:18 -0500

    hola 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Usuario:
     def __init__(self, name: str, edad: int, sexo: str, telefono: str, email: str):
-        self.name: str = edad
+        self.name: str = name
         self.edad: int = edad
         self.sexo: str = sexo
         self.telefono: str = telefono
@@ -20,24 +20,35 @@ class Room:
         return f"Room ID: {self.id}, Tipo: {self.tipo}, Precio: {self.precio}, Características: {self.caracteristicas}"
     
 class Motel:
-    def __init__(self, usuario: Usuario, rooms: list[Room]):
+    def __init__(self, usuario: Usuario, rooms: List[Room]):
         self.usuario: Usuario = usuario
         self.rooms: list[Room] = rooms
         
 if __name__ == "__main__":
-    with open("data.json", "r") as file:
+    with open("datos.json", "r") as file:
         data = json.load(file)
         
     usuario = Usuario(
-        name=data["name"],
-        edad=data["edad"],
-        sexo=data["sexo"],
-        telefono=data["telefono"],
-        email=data["email"]
+        name=data["usuario"]["name"],
+        edad=data["usuario"]["edad"],
+        sexo=data["usuario"]["sexo"],
+        telefono=data["usuario"]["telefono"],
+        email=data["usuario"]["email"]
     )
     
-    motel = Motel(usuario=usuario)
+    room = Room(
+        id=data["rooms"][0]["id"],
+        tipo=data["rooms"][0]["tipo"],
+        precio=data["rooms"][0]["precio"],
+        caracteristicas=data["rooms"][0]["caracteristicas"]
+    )
+    
+    
+    motel = Motel(usuario=usuario, rooms=[room])
     
     print(f"Usuario: {motel.usuario.name}, Edad: {motel.usuario.edad}, Sexo: {motel.usuario.sexo}, Telefono: {motel.usuario.telefono}, Email: {motel.usuario.email}")
     
-    
+usuarios = Usuario(name="Maria", edad=30, sexo="Femenino", telefono="1234567890", email="maria@example.com")
+room1 = Room(id=1, tipo="Sencilla", precio=50.0, caracteristicas=["Cama individual", "Baño privado"])
+room2 = Room(id=2, tipo="Doble", precio=80.0, caracteristicas=["Cama matrimonial", "Baño privado", "TV"])
+print(room1)
